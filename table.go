dc2efefbad16d5d84dad60493df7eb5868b131d4
Sean Moakes <mail@seanmoakes.com>
2025-02-17 11:37:06 +0000

    added printing for multi line cells

--- table.go
+++ table.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 type Table struct {
 	Rows [][]string
@@ -16,6 +20,15 @@ func (t *Table) AddRow(row []string) {
 	t.Rows = append(t.Rows, row)
 }
 
+func isNumeric(s string) bool {
+	for _, char := range s {
+		if !unicode.IsNumber(char) {
+			return false
+		}
+	}
+	return true
+}
+
 func (t *Table) CalculateColumnWidths() []int {
 	if len(t.Rows) == 0 {
 		return nil
@@ -24,12 +37,57 @@ func (t *Table) CalculateColumnWidths() []int {
 	colWidths := make([]int, len(t.Rows[0]))
 	for _, row := range t.Rows {
 		for i, cell := range row {
-			if len(cell) > colWidths[i] {
-				colWidths[i] = len(cell)
+			for _, line := range strings.Split(cell, "\n") {
+				if len(line) > colWidths[i] {
+					colWidths[i] = len(line)
+				}
 			}
 		}
 	}
 	return colWidths
+}
+
+func printRow(row []string, colWidths []int) {
+	// Check for more than one line in the row
+	isMultiLineRow := false
+	for _, c := range row {
+		if strings.Contains(c, "\n") {
+			isMultiLineRow = true
+		}
+	}
+
+	if !isMultiLineRow {
+		for j, cell := range row {
+			if isNumeric(cell) {
+				fmt.Printf("| %*s ", colWidths[j], cell)
+			} else {
+				fmt.Printf("| %-*s ", colWidths[j], cell)
+			}
+		}
+		// Right edge border
+		fmt.Println("|")
+	} else {
+		printMultiLineRow(row, colWidths)
+	}
+}
+
+func printMultiLineRow(row []string, colWidths []int) {
+	var secondRow []string
+	for j, cell := range row {
+		before, after, _ := strings.Cut(cell, "\n")
+		secondRow = append(secondRow, strings.TrimSpace(after))
+
+		if isNumeric(before) {
+			fmt.Printf("| %*s ", colWidths[j], strings.TrimSpace(before))
+		} else {
+			fmt.Printf("| %-*s ", colWidths[j], strings.TrimSpace(before))
+		}
+	}
+	// Right edge border
+	fmt.Println("|")
+
+	// print the additionnal row
+	printRow(secondRow, colWidths)
 }
 
 func (t *Table) Print() {
@@ -39,11 +97,23 @@ func (t *Table) Print() {
 	}
 
 	colWidths := t.CalculateColumnWidths()
-	for _, row := range t.Rows {
-		for i, cell := range row {
-			// Print cell content with padding
-			fmt.Printf(" %-*s |", colWidths[i], cell)
+	printBorder := func() {
+		for _, width := range colWidths {
+			fmt.Print("+" + strings.Repeat("-", width+2))
 		}
-		fmt.Println() // Move to the next line after printing a row
+		fmt.Println("+")
 	}
+
+	// Top border
+	printBorder()
+	for i, row := range t.Rows {
+		printRow(row, colWidths)
+
+		// Header Separator
+		if i == 0 {
+			printBorder()
+		}
+	}
+	// Bottom border
+	printBorder()
 }
